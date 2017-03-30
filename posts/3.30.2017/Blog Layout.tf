7167b897e36c7a8e597ee3b341826407f0f27d66
Tyler Fryman <tfryman@gmail.com>
2017-03-30 18:14:34 +0000

    Changed name to remove '-'

--- posts/3.30.2017/Blog Layout.tf
+++ posts/3.30.2017/Blog Layout.tf
@@ -1,4 +1,4 @@
-In my last post about the source code I should have mentioned where this blog layout came from. I've always been a big fan of minimalist designs. The following series are (mostly) satire, but as if you overlook the vulgar language, the designs are truly minimalistic and I think quite beautiful. 
+In my last post about the source code I should have mentioned where this blog layout came from. I've always been a big fan of minimalist design, art, websites, etc. The following sites are (mostly) satire and if you overlook the vulgar language the designs are truly minimalistic and I think quite beautiful. 
 
  It all started with [MotherFuckingWebsite.com](http://motherfuckingwebsite.com/) and incrementally *"improved"* upon by:
  
@@ -10,4 +10,4 @@ In my last post about the source code I should have mentioned where this blog la
  
 And then there's the one to rule them all [TheBestMotherFuckingWebsite.com](http://thebestmotherfuckingwebsite.com), which I'm sure we all can agree is light-years ahead of any of the others.
 
-I used the [BestMotherFucking.website](https://bestmotherfucking.website), but added my name in a centered &lt;H1&gt; tag at the top and changed all the post headers to &lt;H2&gt; tags with&lt;HR&gt;s between each post.
+I based this blog off of [BestMotherFucking.website](https://bestmotherfucking.website), but added my name in a centered &lt;H1&gt; tag at the top and changed all the post headers to &lt;H2&gt; tags with&lt;HR&gt;s between each post to clearly separate each posts.
